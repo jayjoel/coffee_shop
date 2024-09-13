2d58202e45ec74e7afa1546557fafcca23f9c10b
joel_patrick <jayjoelpn@gmail.com>
2024-09-13 09:48:34 +0300

    test_order

--- tests/test_order.py
+++ tests/test_order.py
@@ -6,21 +6,21 @@ from order import Order
 def test_order_price_validation():
     customer = Customer("Jay")
     with pytest.raises(ValueError):
-        Order(customer, "Cappuccino", 0.9)
+        Order(customer, "Cappuccino", 1.0)
     with pytest.raises(ValueError):
-        Order(customer, "Cappuccino", 10.1)
-    Order(customer, "Cappuccino", 5.0)
+        Order(customer, "Cappuccino", 9.1)
+    Order(customer, "Cappuccino", 6.0)
 
 def test_order_properties():
     customer = Customer("Jay")
-    order = Order(customer, "Cappuccino", 3.5)
+    order = Order(customer, "Cappuccino", 4.5)
     assert order.customer == customer
     assert order.coffee == "Cappuccino"
-    assert order.price == 3.5
+    assert order.price == 4.5
 
 def test_order_all_orders():
     customer1 = Customer("Jay")
     customer2 = Customer("JaneDoe")
-    order1 = Order(customer1, "Cappuccino", 3.5)
+    order1 = Order(customer1, "Cappuccino", 4.5)
     order2 = Order(customer2, "Latte", 4.0)
     
